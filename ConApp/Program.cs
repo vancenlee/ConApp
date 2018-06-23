3ea51cc3f3d72eb0d516453e2047e0667a4fb475
vancenlee <vancenlee@126.com>
2018-06-23 22:30:04 +0800

    创建项目

---
+++ ConApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
